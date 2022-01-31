7afac8eb44719a087647dec3d2da6fff4196663c
annagreeneee <88964564+annagreeneee@users.noreply.github.com>
2022-01-30 22:10:52 -0500

    Delete Module3_AnnaR.R

--- Module3_AnnaR.R
+++
@@ -1,9 +0,0 @@
-Name <- c("Jeb" , "Donald" , "Ted”, “Marco”, “Carly”, “Hillary”, “Berine”)
-ABC <- c(4, 62 51, 21, 2, 14, 15)
-NCB <- c(12, 75, 43, 19, 1, 21, 19)
-results <- cbind(Name, ABC, NCB)
-results
-results.df <- data.frame(Name, ABC, NCB)
-results.df
-mean(results.df[,2])
-mean(results.df[,3])
